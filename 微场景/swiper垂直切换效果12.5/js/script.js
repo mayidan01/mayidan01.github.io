b5ae5b63089cabb0e5ae9aa5063805671bd4454b
mayidan01 <1265221680@qq.com>
2017-12-08 08:51:18 +0800

    第一屏动画

--- 微场景/swiper垂直切换效果12.5/js/script.js
+++ 微场景/swiper垂直切换效果12.5/js/script.js
@@ -8,6 +8,15 @@
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     },
+     on:{
+      init: function(){
+        swiperAnimateCache(this); //隐藏动画元素 
+        swiperAnimate(this); //初始化完成开始动画
+      }, 
+      slideChangeTransitionEnd: function(){ 
+        swiperAnimate(this); //每个slide切换结束时也运行当前slide动画
+      } 
+    }
     
     });
- 
+  
