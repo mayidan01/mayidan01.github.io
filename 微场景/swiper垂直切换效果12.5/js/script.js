be5d68130434b712707443dce23ccce2fa0b0303
mayidan01 <1265221680@qq.com>
2017-12-11 10:25:56 +0800

    新增音乐1

--- 微场景/swiper垂直切换效果12.5/js/script.js
+++ 微场景/swiper垂直切换效果12.5/js/script.js
@@ -20,3 +20,19 @@
     
     });
   
+//var mp3btn = {
+//	changeClass: function (target,id) {
+//       	var className = $(target).attr('class');
+//       	var ids = document.getElementById(id);
+//       	(className == 'on')
+//           	? $(target).removeClass('on').addClass('off')
+//           	: $(target).removeClass('off').addClass('on');
+//       	(className == 'on')
+//           	? ids.pause()
+//           	: ids.play();
+//   	},
+//	play:function(){
+//		document.getElementById('media').play();
+//	}
+//}
+//mp3btn.play();
